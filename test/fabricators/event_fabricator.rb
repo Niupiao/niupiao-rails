d854f84e2ae9bb8014f0652c6377ff7b0bcb2cb9
Kevin Chen <meejathoughts@gmail.com>
2015-02-17 14:05:13 -0500

    seeding our test photos

--- test/fabricators/event_fabricator.rb
+++ test/fabricators/event_fabricator.rb
@@ -4,7 +4,7 @@ Fabricator(:event) do
   date           { Date.today }
   location       { "#{Faker::Address.city}" }
   description    { "an awesome concert" }
-  link           { "event url" }
+  link           { ['/assets/tiesto.jpg', '/assets/daft_punk.jpg'].sample }
   total_tickets  { (100..200).to_a.sample }
   tickets_sold   { (50..75).to_a.sample }
 end
