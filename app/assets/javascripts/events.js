db8805de8ac1a11dd7773312693376ca3faf1fbf
kevinchen93 <meejathoughts@gmail.com>
2015-02-16 23:46:35 -0500

    added sweet javascript; JS should be in separate events.js file though

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,19 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+  //Direction aware image gallery effect
+  //created by http://8bit-code.com
+//tutorial at http://www.8bit-code.com/tutorials/direction-aware-image-gallery-effect
+
 $(function () {
     $(".gallery li").on("mouseenter mouseleave", function(e){
 
