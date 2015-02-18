5a162aaab2c7a6c4da0c286d37ca9fc970c41b7c
Kevin Chen <meejathoughts@gmail.com>
2015-02-18 08:37:28 -0500

    Skipping check for authenticity token so mobile app can POST for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
 
   include ApplicationHelper
 
+  def json_request?
+    request.format.json?
+  end
+  
   def ensure_auth
     logged_in? || ensure_token
   end
