b431ea9ed15855d5a07935d1abbde838d86729ff
kevinchen93 <meejathoughts@gmail.com>
2015-03-15 15:55:26 -0400

    updated ticket#create test

--- test/integration/tickets_test.rb
+++ test/integration/tickets_test.rb
@@ -4,8 +4,23 @@ class TicketsTest < ActionDispatch::IntegrationTest
   include IntegrationHelperTest
 
   def setup
-    @event = Fabricate(:event)
-    @user = User.create(username: 'kmc3', email: 'kmc3@williams.edu', password: 'foobar')
+    @event = Event.create!(
+                           name: 'TestEvent',
+                           organizer: 'TestOrganizer',
+                           location: 'Williamstown, MA',
+                           description: 'TestDescription',
+                           image_path: 'TestImagePath',
+                           link: 'TestLink',
+                           total_tickets: 0,
+                           tickets_sold: 0
+                           )
+    @general = TicketStatus.create!(name: "General", max_purchasable: 3)
+    @vip = TicketStatus.create!(name: "VIP", max_purchasable: 2)
+    @event.ticket_statuses << @general
+    @event.ticket_statuses << @vip
+    @event.save!
+
+    @user = User.create(username: 'kmc3', email: 'kmc3@williams.edu', password: 'foobar', name: 'Kevin Chen', first_name: 'Kevin', last_name: 'Chen')
     login @user
   end
 
@@ -15,7 +30,7 @@ class TicketsTest < ActionDispatch::IntegrationTest
         event_id: @event.id,
         user_id: @user.id,
         price: 100,
-        status: Ticket::STATUS_VIP
+        ticket_status_id: @general.id
       }
     end
   end
