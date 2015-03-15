dc45a648d49c7dd848c698b2cf28c0f56cb8da0d
Kevin Chen <meejathoughts@gmail.com>
2015-03-14 22:23:20 -0400

    fixed typo

--- db/migrate/20150314234006_create_ticket_statuses.rb
+++ db/migrate/20150314234006_create_ticket_statuses.rb
@@ -2,7 +2,7 @@ class CreateTicketStatuses < ActiveRecord::Migration
   def change
     create_table :ticket_statuses do |t|
       t.string :name
-      t.integer :max_purchaseable
+      t.integer :max_purchasable
       t.references :event, index: true
 
       t.timestamps null: false
