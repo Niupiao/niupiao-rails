57f5d07e2f574a26848543a84758464fecfa6a0e
tempity <rhk1@williams.edu>
2015-04-13 00:39:55 +0000

    Basic pre-functionality work is done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
-
   get 'static_pages/about'
 
-  root 'sessions#new'
+  root 'static_pages#home'
 
   get 'login',     to: 'sessions#new'
   post 'login',    to: 'sessions#create'
