b8cfe27320223dc97109557e19da60c27159b9d4
rynkwn <rynkwn@gmail.com>
2015-04-14 00:53:47 +0000

    BUGGY: Process began for adding ticket statuses to event creation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -97,3 +97,16 @@ $(function () {
                                                                                                                                                       });
 
                                                                                                                                                       });
+
+function remove_ticket_status(link) {
+  $(link).previous("input[type=hidden]").value = "1";
+  $(link).up(".ticket_status").hide();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime(); //Used to get a unique id.
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).up().insert({
+    before: content.replace(regexp, new_id)
+  });
+}
